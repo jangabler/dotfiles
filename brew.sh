04b3cc0f52fc42d10d60ea77e9759eb2d834b381
jangabler <1701963+jangabler@users.noreply.github.com>
2019-10-30 21:34:24 +0100

    Update brew.sh file with some casks

--- brew.sh
+++ brew.sh
@@ -4,11 +4,18 @@ brew update
 
 brew upgrade
 
+brew install git
 brew install gnupg
 brew install go
 brew install htop
 brew install nvm
 brew install php
 brew install sass
+
+brew cask install docker
+brew cask install filezilla
+brew cask install firefox
+brew cask install keepassxc
+brew cask install phpstorm
 
 brew cleanup
