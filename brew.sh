be20a73ba59676a33ea3f306d121993ae65ed8ac
jangabler <1701963+jangabler@users.noreply.github.com>
2019-11-02 00:30:38 +0100

    Update brew.sh file with some casks III

--- brew.sh
+++ brew.sh
@@ -13,13 +13,19 @@ brew install php
 brew install sass
 
 brew cask install docker
+brew cask install dropbox
 brew cask install filezilla
 brew cask install firefox
 brew cask install goland
+brew cask install google-chrome
 brew cask install keepassxc
+brew cask install miktex-console
+brew cask install mysqlworkbench
 brew cask install phpstorm
+brew cask install spotify
 brew cask install sublime-text
 brew cask install vagrant
 brew cask install virtualbox
+brew cask install whatsapp
 
 brew cleanup
