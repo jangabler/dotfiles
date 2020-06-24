aff7b3db88de61648abed67561c8ba1f4e147bc3
jangabler <1701963+jangabler@users.noreply.github.com>
2020-06-24 17:09:19 +0200

    Update brew.sh file with font-jetbrains-mono and netbeans

--- brew.sh
+++ brew.sh
@@ -20,11 +20,13 @@ brew cask install docker
 brew cask install dropbox
 brew cask install filezilla
 brew cask install firefox
+brew cask install font-jetbrains-mono
 brew cask install goland
 brew cask install google-chrome
 brew cask install keepassxc
 brew cask install miktex-console
 brew cask install mysqlworkbench
+brew cask install netbeans
 brew cask install netnewswire
 brew cask install phpstorm
 brew cask install spotify
