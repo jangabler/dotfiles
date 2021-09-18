ce709eee1640f8a74b9582183a92bb70760de432
jangabler <1701963+jangabler@users.noreply.github.com>
2021-09-18 14:15:48 +0200

    Update npm.sh file and delete global create-react-app installation

--- installation/npm.sh
+++ installation/npm.sh
@@ -3,7 +3,6 @@
 npm install --global @prettier/plugin-php
 npm install --global @prettier/plugin-pug
 npm install --global @prettier/plugin-xml
-npm install --global create-react-app
 npm install --global prettier
 npm install --global typescript
 npm install --global yarn
