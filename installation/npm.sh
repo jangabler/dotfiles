74c9e0a7b1c63faff876f161a7d01200982fce3c
jangabler <1701963+jangabler@users.noreply.github.com>
2021-11-05 20:09:10 +0100

    Update npm.sh file with global jstransformer-markdown-it and pug-cli installation

--- installation/npm.sh
+++ installation/npm.sh
@@ -3,7 +3,9 @@
 npm install --global @prettier/plugin-php
 npm install --global @prettier/plugin-pug
 npm install --global @prettier/plugin-xml
+npm install --global jstransformer-markdown-it
 npm install --global prettier
+npm install --global pug-cli
 npm install --global typescript
 npm install --global yarn
 
