ebf909d22cc1a87a36a33e80cc341586634d4022
jangabler <1701963+jangabler@users.noreply.github.com>
2022-01-29 12:58:14 +0100

    Add visual studio code settings.json and extension installation script

---
+++ installation/vscode.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+code --install-extension BeardedBear.beardedtheme
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.copilot
+code --install-extension GitHub.github-vscode-theme
+code --install-extension MS-CEINTL.vscode-language-pack-de
+code --install-extension Pivotal.vscode-boot-dev-pack
+code --install-extension Pivotal.vscode-spring-boot
+code --install-extension redhat.java
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vscjava.vscode-spring-boot-dashboard
+code --install-extension vscjava.vscode-spring-initializr
